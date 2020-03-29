741b1f5edff1b22c9183f8762a93f8734f52056a
ksh980212 <ksh980212@gmail.com>
2020-03-29 19:37:36 +0900

    Initial commit

---
+++ src/main/java/app/springboot/SpringbootApplication.java
@@ -0,0 +1,16 @@
+package app.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(SpringbootApplication.class);
+		app.setWebApplicationType(WebApplicationType.NONE);
+		app.run(args);
+	}
+
+}
