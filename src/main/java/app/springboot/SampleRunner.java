b092d1da4cf239b20db6a32e692a71fc0162b36b
ksh980212 <ksh980212@gmail.com>
2020-03-31 20:02:21 +0900

    [외부설정1ë] Property

---
+++ src/main/java/app/springboot/SampleRunner.java
@@ -0,0 +1,25 @@
+package app.springboot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SampleRunner implements ApplicationRunner {
+
+	@Value("${seungho.name}")
+	private String name;
+
+	@Value("${seungho.age}")
+	private int age;
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		System.out.println("================");
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println("================");
+	}
+
+}
--- src/test/java/app/springboot/SpringbootApplicationTests.java
+++ src/test/java/app/springboot/SpringbootApplicationTests.java
@@ -1,13 +1,29 @@
 package app.springboot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.TestPropertySource;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+//@TestPropertySource(properties = {"seungho.name=hoho", "seungho.age=23"})
+@TestPropertySource(locations = "classpath:/test.properties")
 @SpringBootTest
 class SpringbootApplicationTests {
 
+	@Autowired
+	private Environment environment;
+
 	@Test
 	void contextLoads() {
+//		assertThat(environment.getProperty("seungho.name"))
+////				.isEqualTo("seungho");
+//		assertThat(environment.getProperty("seungho.name"))
+//				.isEqualTo("hoho");
+		assertThat(environment.getProperty("seungho.name"))
+				.isEqualTo("good");
 	}
 
 }
