a69e42d2dc7fa707efb350b2b43ff48a68924ecf
ksh980212 <ksh980212@gmail.com>
2020-04-06 14:39:09 +0900

    [Test] - ControllerTest (WebEnvironment.RANDOM_PORT) + MockBean

--- src/test/java/app/springboot/sample/SampleControllerTest.java
+++ src/test/java/app/springboot/sample/SampleControllerTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -15,13 +17,18 @@ public class SampleControllerTest {
 	@Autowired
 	private TestRestTemplate testRestTemplate;
 
+	@MockBean
+	private SampleService mockSampleService;
+
 	@Test
 	public void hello() throws Exception {
+		when(mockSampleService.getName()).thenReturn("whiteship");
+
 		String result = testRestTemplate.getForObject("/hello", String.class);
-		assertThat(result).isEqualTo("hello seungho");
+		assertThat(result).isEqualTo("hello whiteship");
 	}
 
 	/**
-	 * 이 상황에서는 테스트가 서비스 단까지 가게된다.
+	 * 컨트롤러만 분리해서 테스트하고 싶다 => MockBean
 	 */
 }
