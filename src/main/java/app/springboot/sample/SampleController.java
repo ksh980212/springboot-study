784773d22a6e8982eaf7bbbbaf3b68673c0cd6a0
ksh980212 <ksh980212@gmail.com>
2020-04-06 15:03:10 +0900

    [Test] - Logger 테스트

--- src/main/java/app/springboot/sample/SampleController.java
+++ src/main/java/app/springboot/sample/SampleController.java
@@ -1,6 +1,8 @@
 package app.springboot.sample;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleController {
 
 	private final SampleService sampleService;
+	private Logger logger = LoggerFactory.getLogger(SampleController.class);
 
 	@GetMapping("/hello")
 	public String hello() {
+		logger.info("will");
+		logger.info("skip");
 		return "hello " + sampleService.getName();
 	}
 
--- src/test/java/app/springboot/sample/SampleControllerTest.java
+++ src/test/java/app/springboot/sample/SampleControllerTest.java
@@ -1,16 +1,21 @@
 package app.springboot.sample;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith(OutputCaptureExtension.class)
 @WebMvcTest(SampleController.class) // Controller와 다른 계층의 의존성 분리
 public class SampleControllerTest {
 
@@ -21,12 +26,16 @@ public class SampleControllerTest {
 	private MockMvc mockMvc;
 
 	@Test
-	public void hello() throws Exception {
+	public void hello(CapturedOutput capturedOutput) throws Exception {
 		when(mockSampleService.getName()).thenReturn("whiteship");
 
 		mockMvc.perform(get("/hello"))
 				.andExpect(status().isOk())
 				.andExpect(content().string("hello whiteship"));
+
+		assertThat(capturedOutput.toString())
+				.contains("will")
+				.contains("skip");
 	}
 
 }
