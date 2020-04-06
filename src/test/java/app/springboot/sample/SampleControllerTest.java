ea076224715aeeffac2d656ae9b2226e47580747
ksh980212 <ksh980212@gmail.com>
2020-04-06 14:50:16 +0900

    [Test] - ControllerTest WebMvcTest(의존성 분리)

--- src/test/java/app/springboot/sample/SampleControllerTest.java
+++ src/test/java/app/springboot/sample/SampleControllerTest.java
@@ -2,30 +2,31 @@ package app.springboot.sample;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
+@WebMvcTest(SampleController.class) // Controller와 다른 계층의 의존성 분리
 public class SampleControllerTest {
-
-	@Autowired
-	private WebTestClient webTestClient; // 비동기로 동작됨.
 
 	@MockBean
 	private SampleService mockSampleService;
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	public void hello() throws Exception {
 		when(mockSampleService.getName()).thenReturn("whiteship");
 
-		webTestClient.get().uri("/hello").exchange()
-				.expectStatus().isOk()
-				.expectBody(String.class).isEqualTo("hello whiteship");
+		mockMvc.perform(get("/hello"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("hello whiteship"));
 	}
 
 }
