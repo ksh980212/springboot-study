31366bcd4efff09e9818de07aa1deb972adb9361
ksh980212 <ksh980212@gmail.com>
2020-04-12 18:21:03 +0900

    [Spring-boot-MVC] 정적 리스소 지원(Static Resources)

--- src/main/java/app/springboot/SpringbootApplication.java
+++ src/main/java/app/springboot/SpringbootApplication.java
@@ -3,6 +3,20 @@ package app.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *  Static Reources Mapping
+ *  기본 Resource 위치
+ *  classpath:/static
+ *  classpath:/public
+ *  classpath:/resoruces/
+ *  classpath:/META-INF/resources
+ */
+
+/**
+ * HTTP STATUS CODE 304 vs 200
+ * Last-Modified 헤더를 보고 304 응답을 보냄.
+ */
+
 @SpringBootApplication
 public class SpringbootApplication {
 
--- src/main/java/app/springboot/config/WebConfig.java
+++ src/main/java/app/springboot/config/WebConfig.java
@@ -1,10 +1,21 @@
 package app.springboot.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
+	/**
+	 * WebMvcConfigurer의 addResourceHandlers로 커스터마이징 할 수 있다.
+	 * 캐싱 전략을 따로 설정해주어야 한다. setCachedPeriod(20)
+	 */
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/m/**")
+				.addResourceLocations("classpath:/m/")
+				.setCachePeriod(20);
+	}
+
 }
