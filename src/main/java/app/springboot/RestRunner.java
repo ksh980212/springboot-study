18d495c5973e5c3456b03ab408e7fe9ac6f0a82a
ksh980212 <ksh980212@gmail.com>
2020-04-21 21:17:10 +0900

    [Rest-Client] 커스터마이징

--- src/main/java/app/springboot/RestRunner.java
+++ src/main/java/app/springboot/RestRunner.java
@@ -3,6 +3,7 @@ package app.springboot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -12,20 +13,31 @@ import reactor.core.publisher.Mono;
 public class RestRunner implements ApplicationRunner {
 
 	@Autowired
-	private WebClient.Builder build;
+	private WebClient.Builder builder;
+
+	@Autowired
+	private RestTemplateBuilder restTemplateBuilder;
+
+	private WebClient webClient;
+
+	public RestRunner(WebClient.Builder build) {
+		this.webClient = build.build();
+	}
 
 	/**
 	 * WebClient: Non-Blocking I/O 기반의 Asynchronous API
 	 */
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		WebClient webClient = build.build();
+		WebClient webClient = builder
+				.baseUrl("http://localhost:8080")
+				.build();
 
 		StopWatch stopWatch = new StopWatch();
 		stopWatch.start();
 
 		// hello
-		Mono<String> helloMono = webClient.get().uri("http://localhost:8080/hello")
+		Mono<String> helloMono = webClient.get().uri("/hello")
 				.retrieve()
 				.bodyToMono(String.class);
 
@@ -38,7 +50,7 @@ public class RestRunner implements ApplicationRunner {
 		});
 
 		// world
-		Mono<String> worldMono = webClient.get().uri("http://localhost:8080/world")
+		Mono<String> worldMono = webClient.get().uri("/world")
 				.retrieve()
 				.bodyToMono(String.class);
 		worldMono.subscribe(s -> {
--- src/main/java/app/springboot/SpringbootApplication.java
+++ src/main/java/app/springboot/SpringbootApplication.java
@@ -2,12 +2,32 @@ package app.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 
 @SpringBootApplication
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+	@Bean
+	public WebClientCustomizer webClientCustomizer() {
+		return webClientBuilder -> webClientBuilder.baseUrl("http://localhost:8080");
+//		return new WebClientCustomizer() {
+//			@Override
+//			public void customize(WebClient.Builder webClientBuilder) {
+//				webClientBuilder.baseUrl("http://localhost:8080");
+//			}
+//		};
+	}
+
+	@Bean
+	public RestTemplateCustomizer restTemplateCustomizer() {
+		return restTemplate -> restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
 	}
 
 }
