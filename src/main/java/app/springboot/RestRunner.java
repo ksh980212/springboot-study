4131a33dee3407a76a9358009ddd3cda1cba223a
ksh980212 <ksh980212@gmail.com>
2020-04-21 21:11:10 +0900

    [Rest-Client] WebClient - Non-Blocking I/O기반의 Asynchronous API

--- src/main/java/app/springboot/RestRunner.java
+++ src/main/java/app/springboot/RestRunner.java
@@ -3,36 +3,51 @@ package app.springboot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Component
 public class RestRunner implements ApplicationRunner {
 
 	@Autowired
-	private RestTemplateBuilder restTemplateBuilder;
+	private WebClient.Builder build;
 
 	/**
-	 * RestTemplate: Blocking I/O기반의 Synchronous API
+	 * WebClient: Non-Blocking I/O 기반의 Asynchronous API
 	 */
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		RestTemplate restTemplate = restTemplateBuilder.build();
+		WebClient webClient = build.build();
 
 		StopWatch stopWatch = new StopWatch();
 		stopWatch.start();
 
 		// hello
-		String helloResult = restTemplate.getForObject("http://localhost:8080/hello", String.class);
-		System.out.println(helloResult);
+		Mono<String> helloMono = webClient.get().uri("http://localhost:8080/hello")
+				.retrieve()
+				.bodyToMono(String.class);
+
+		helloMono.subscribe(s -> {
+			System.out.println(s);
+			if (stopWatch.isRunning()) {
+				stopWatch.stop();
+			}
+			System.out.println(stopWatch.prettyPrint());
+		});
 
 		// world
-		String worldResult = restTemplate.getForObject("http://localhost:8080/world", String.class);
-		System.out.println(worldResult);
-
-		stopWatch.stop();
+		Mono<String> worldMono = webClient.get().uri("http://localhost:8080/world")
+				.retrieve()
+				.bodyToMono(String.class);
+		worldMono.subscribe(s -> {
+			System.out.println(s);
+			if (stopWatch.isRunning()) {
+				stopWatch.stop();
+			}
+			System.out.println(stopWatch.prettyPrint());
+		});
 		System.out.println(stopWatch.prettyPrint());
 	}
 
